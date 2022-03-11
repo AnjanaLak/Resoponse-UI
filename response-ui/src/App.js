e040623cad59795b2b6f2066c61306615f4a165b
AnjanaLak <hkanjanalakshan95@gmail.com>
2022-03-11 23:25:46 +0530

    Folder structure established with related components

---
+++ response-ui/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import {Footer, Blog, Features, Header, Possibility, WhatGPT3} from './containers';
+import {Article, Brand, CTA, Feature, Navbar} from './components';
+
+const App = () => {
+    return (
+
+        <div className='App'>
+           <div className='gradient__bg'>
+               
+           </div>
+        </div>
+    )
+}
+ 
+export default App
